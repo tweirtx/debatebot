7c5e08e8116252e279b226c13781dae5e9033bea
Travis Weir <tweirtx@gmail.com>
2018-06-19 23:20:31 -0500

    pylinted and found many mistakes

--- debatebot.py
+++ debatebot.py
@@ -1,30 +1,32 @@
-import discord
+"""A Discord bot for structuring Discord debates"""
 import os
 import json
+import discord
+from discord.ext.commands import has_permissions, bot_has_permissions
 import db
-import time
-from discord.ext.commands import Bot, has_permissions, bot_has_permissions
 
-config = {
+
+CONFIG = {
     'discord_token': "Put Discord API Token here.",
 }
-config_file = 'config.json'
+CONFIG_FILE = 'CONFIG.json'
 
-if os.path.isfile(config_file):
-    with open(config_file) as f:
-        config.update(json.load(f))
+if os.path.isfile(CONFIG_FILE):
+    with open(CONFIG_FILE) as f:
+        CONFIG.update(json.load(f))
 
-with open('config.json', 'w') as f:
-    json.dump(config, f, indent='\t')
+with open('CONFIG.json', 'w') as f:
+    json.dump(CONFIG, f, indent='\t')
 
 
-bot = discord.ext.commands.Bot(command_prefix='d!')
+BOT = discord.ext.commands.Bot(command_prefix='d!')
 
 
 @bot_has_permissions(manage_channels=True, manage_roles=True)
 @has_permissions(manage_channels=True)
-@bot.command()
+@BOT.command()
 async def create(ctx, name, side1, side2):
+    """Sets up a debate"""
     with db.Session() as session:
         already_debate = session.query(Storage).filter_by(guild=ctx.guild.id).one_or_none()
         if already_debate is not None:
@@ -50,12 +52,15 @@ async def create(ctx, name, side1, side2):
         await side2_channel.set_permissions(target=ctx.guild.default_role, read_messages=False)
         side2_channel.overwrites_for(side2_role).update(read_messages=True, send_messages=True)
 
-        debate = Storage(guild=ctx.guild.id, side1_role=side1_role.id, side2_role=side2_role.id, main_channel=main_channel.id,
-                                                                            side1_channel=side1_channel.id,
-                                                                            side2_channel=side2_channel.id,
-                                                                            side1_name=side1,
-                                                                            side2_name=side2,
-                                                                            admin=ctx.author.id)
+        debate = Storage(guild=ctx.guild.id,
+                         side1_role=side1_role.id,
+                         side2_role=side2_role.id,
+                         main_channel=main_channel.id,
+                         side1_channel=side1_channel.id,
+                         side2_channel=side2_channel.id,
+                         side1_name=side1,
+                         side2_name=side2,
+                         admin=ctx.author.id)
         session.add(debate)
 
     await ctx.send("Debate {} with sides {} and {} on the other created successfully".format(name, side1, side2))
@@ -63,8 +68,9 @@ async def create(ctx, name, side1, side2):
 
 
 @bot_has_permissions(manage_roles=True)
-@bot.command()
+@BOT.command()
 async def floor(ctx, *, side):
+    """Gives the floor to one side"""
     with db.Session() as session:
         is_admin = session.query(Storage).filter_by(admin=ctx.author.id, guild=ctx.guild.id).one_or_none()
         if is_admin is None:
@@ -90,8 +96,9 @@ async def floor(ctx, *, side):
             await ctx.send("No side with that found! Please try again!")
 
 
-@bot.command()
+@BOT.command()
 async def join(ctx, side: discord.Role):
+    """Lets a user join a side"""
     with db.Session() as session:
         side1 = session.query(Storage).filter_by(side1_name=side).one_or_none()
         side2 = session.query(Storage).filter_by(side2_name=side).one_or_none()
@@ -112,8 +119,9 @@ async def join(ctx, side: discord.Role):
         await ctx.send("Successfully joined you to {} side!".format(side))
 
 
-@bot.command()
+@BOT.command()
 async def end(ctx):
+    """Ends a debate"""
     with db.Session() as session:
         active_debate = session.query(Storage).filter_by(guild=ctx.guild.id).one_or_none()
         if active_debate is None:
@@ -124,7 +132,7 @@ async def end(ctx):
             return
         else:
             # discord.CategoryChannel(data=active_debate.channelcat, guild=ctx.guild, state=True)
-            cat_channel = bot.get_channel(active_debate.main_channel).category
+            cat_channel = BOT.get_channel(active_debate.main_channel).category
             for i in ctx.guild.roles:
                 if i.id == active_debate.side1_role:
                     role1 = i
@@ -147,22 +155,26 @@ async def end(ctx):
             await ctx.send("Debate ended successfully!")
 
 
-@bot.command()
+@BOT.command()
 async def feedback(ctx):
+    """Links to the feedback form"""
     await ctx.send("Want to give feedback on the bot? Go here: https://goo.gl/forms/EyEmiCbvKhA3Ngz22")
 
 
-@bot.command()
+@BOT.command()
 async def github(ctx):
+    """Links to source code"""
     await ctx.send("Check out my bot code! You can see it here: https://github.com/tweirtx/DebateBot")
 
 
-@bot.event
+@BOT.event
 async def on_ready():
+    """Tells the host that it's ready"""
     print("Ready!")
 
 
 class Storage(db.DatabaseObject):
+    """Stores everything"""
     __tablename__ = 'debateStorage'
     guild = db.Column(db.Integer, primary_key=True)
     side1_role = db.Column(db.Integer)
@@ -175,4 +187,4 @@ class Storage(db.DatabaseObject):
     admin = db.Column(db.Integer)
 
 
-bot.run(config['discord_token'])
+BOT.run(CONFIG['discord_token'])
